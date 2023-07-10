cc7e6a53dd9130d80ed59b34a560507c6b339a2e
becky-ho <138867937+becky-ho@users.noreply.github.com>
2023-07-10 15:11:05 +0800

    Add files via upload

--- pythonProject/main1.py
+++ pythonProject/main1.py
@@ -2,17 +2,21 @@
 # 國泰銀行要慶祝六十周年，需要買字母貼紙來布置活動空間，文字為"Hello welcome to Cathay 60th year anniversary"，
 # 請寫一個程式計算每個字母(大小寫視為同個字母)出現次
 
-
-
-from collections import Counter
-
 def count_letters(text):
-    text = text.lower()  # 將文字轉換為小寫
-    letter_count = Counter(c for c in text if c.isalpha())
-    return letter_count
+    letter_count = {}
+    for char in text:
+        if char.isalnum():
+            char = char.lower()
+            if char in letter_count:
+                letter_count[char] += 1
+            else:
+                letter_count[char] = 1
+
+    sorted_count = sorted(letter_count.items())
+    return sorted_count
 
 # 測試程式
 text = "Hello welcome to Cathay 60th year anniversary"
 result = count_letters(text)
-for letter, count in result.items():
-    print(letter, ":", count)
+for letter, count in result:
+    print(f"{letter}: {count}")
